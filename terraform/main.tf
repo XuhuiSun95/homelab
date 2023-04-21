f09918886680f9611972ca303e65edc30b673737
XuhuiSun95 <ericsun1995@gmail.com>
2023-04-21 14:58:42 -0700

    :sparkles: add terraform pve

---
+++ terraform/main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "2.9.14"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = "https://192.168.1.27:8006/api2/json"
+}
+
+resource "proxmox_vm_qemu" "master-01" {
+  target_node = "pve1"
+  vmid        = 100
+  name        = "master-01.xuhuisun.local"
+  desc        = "k8s master node 01"
+
+  clone = "ubuntu-cloud"
+
+  memory  = 3840
+  sockets = 1
+  cores   = 1
+  cpu     = ""
+  scsihw  = "virtio-scsi-pci"
+  disk {
+    type    = "scsi"
+    storage = "local-lvm"
+    size    = "10G"
+  }
+  ipconfig0 = "ip=192.168.1.250/24,gw=192.168.1.1"
+}
+
+resource "proxmox_vm_qemu" "master-02" {
+  target_node = "pve1"
+  vmid        = 101
+  name        = "master-02.xuhuisun.local"
+  desc        = "k8s master node 02"
+
+  clone = "ubuntu-cloud"
+
+  memory  = 3840
+  sockets = 1
+  cores   = 1
+  cpu     = ""
+  scsihw  = "virtio-scsi-pci"
+  disk {
+    type    = "scsi"
+    storage = "local-lvm"
+    size    = "10G"
+  }
+  ipconfig0 = "ip=192.168.1.251/24,gw=192.168.1.1"
+}
+
+resource "proxmox_vm_qemu" "master-03" {
+  target_node = "pve1"
+  vmid        = 102
+  name        = "master-03.xuhuisun.local"
+  desc        = "k8s master node 03"
+
+  clone = "ubuntu-cloud"
+
+  memory  = 3840
+  sockets = 1
+  cores   = 1
+  cpu     = ""
+  scsihw  = "virtio-scsi-pci"
+  disk {
+    type    = "scsi"
+    storage = "local-lvm"
+    size    = "10G"
+  }
+  ipconfig0 = "ip=192.168.1.252/24,gw=192.168.1.1"
+}
